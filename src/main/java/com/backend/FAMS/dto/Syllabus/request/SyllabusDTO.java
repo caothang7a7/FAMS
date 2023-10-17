d11e6ed6fbaa5cfd8e8ecc844c6374105abde03e
caothang7a7 <caothang7a7@gmail.com>
2023-10-17 10:17:45 +0700

    fix

---
+++ src/main/java/com/backend/FAMS/dto/Syllabus/request/SyllabusDTO.java
@@ -0,0 +1,11 @@
+package com.backend.FAMS.dto.Syllabus.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SyllabusDTO {
+}
