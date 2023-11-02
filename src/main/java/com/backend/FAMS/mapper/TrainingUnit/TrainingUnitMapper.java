9c9fbb4c3974e12bfc7763761eb37de171064e26
viet-quanggg <nguyenvietquang099@gmail.com>
2023-11-02 13:51:28 +0700

    add file

---
+++ src/main/java/com/backend/FAMS/mapper/TrainingUnit/TrainingUnitMapper.java
@@ -0,0 +1,9 @@
+package com.backend.FAMS.mapper.TrainingUnit;
+
+import org.mapstruct.Mapper;
+import org.springframework.stereotype.Component;
+
+@Mapper(componentModel = "spring")
+@Component
+public interface TrainingUnitMapper {
+}
