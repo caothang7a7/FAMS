6cf6a82b81cc60e87bfc338a0714ccf0d5817850
viet-quanggg <nguyenvietquang099@gmail.com>
2023-11-23 10:20:59 +0700

    resolve

--- src/main/java/com/backend/FAMS/mapper/Syllabus/SyllabusMapper.java
+++ src/main/java/com/backend/FAMS/mapper/Syllabus/SyllabusMapper.java
@@ -2,12 +2,9 @@ package com.backend.FAMS.mapper.Syllabus;
 import com.backend.FAMS.dto.Syllabus.request.SyllabusDTOCreateGeneralRequest;
 import com.backend.FAMS.dto.Syllabus.request.SyllabusOutlineScreen;
 import com.backend.FAMS.dto.Syllabus.request.TrainingUnitDTOCreate;
-<<<<<<< HEAD
 import com.backend.FAMS.dto.Syllabus.response.SyllabusDTOOutline;
 import com.backend.FAMS.dto.Syllabus.response.SyllabusDTOShowGeneral;
-=======
 import com.backend.FAMS.dto.Syllabus.response.*;
->>>>>>> main
 import com.backend.FAMS.dto.trainingContent.TrainingContentDTOCreateOutlineScreen;
 import com.backend.FAMS.entity.Syllabus.Syllabus;
 import com.backend.FAMS.dto.Syllabus.request.SyllabusDTOCreateOtherScreen;
